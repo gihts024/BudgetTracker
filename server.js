0304bd3750fdfa26f6bc031f949456bf6934d91c
Alex Gihts <githenyalex@gmail.com>
2021-10-11 06:12:31 -0700

    mongo config

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/alexbudget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const connection = "mongodb+srv://root:the%23Eagle@cluster0.pgh9o.mongodb.net/alexbudget?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
 
 // routes
 app.use(require("./routes/api.js"));
@@ -26,3 +26,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
